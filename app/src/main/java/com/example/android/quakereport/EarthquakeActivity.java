ef7d0a64e12a5f879024d717ccee3d4afb6ddf38
Miguel Novillo <mnovillodiaz@gmail.com>
2017-07-18 08:53:56 +0200

    Adding a sorting preference

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,10 +17,13 @@ package com.example.android.quakereport;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -35,14 +38,13 @@ import java.util.ArrayList;
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
-    public Context context;
     private ProgressBar progressBar;
     private TextView emptyView;
 
 
     /** URL for earthquake data from the USGS dataset */
     private static final String USGS_REQUEST_URL =
-            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
+            "https://earthquake.usgs.gov/fdsnws/event/1/query";
 
     public EarthquakeTransformer earthquakeTransformer = new EarthquakeTransformer();
 
@@ -72,6 +74,25 @@ public class EarthquakeActivity extends AppCompatActivity {
         progressBar = (ProgressBar) findViewById(R.id.progress_bar);
         emptyView = (TextView) findViewById(R.id.empty_view);
 
+
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String minMagnitude = sharedPrefs.getString(
+                getString(R.string.settings_min_magnitude_key),
+                getString(R.string.settings_min_magnitude_default));
+
+        String orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default));
+
+        Uri baseUri = Uri.parse(USGS_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("format", "geojson");
+        uriBuilder.appendQueryParameter("limit", "10");
+        uriBuilder.appendQueryParameter("minmag", minMagnitude);
+        uriBuilder.appendQueryParameter("orderby", orderBy);
+
         ConnectivityManager cm =
                 (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
@@ -81,7 +102,7 @@ public class EarthquakeActivity extends AppCompatActivity {
 
         if(isConnected) {
             EarthquakeAsyncTask task = new EarthquakeAsyncTask();
-            task.execute(USGS_REQUEST_URL);
+            task.execute(uriBuilder.toString());
         } else {
             progressBar.setVisibility(View.GONE);
             emptyView.setVisibility(View.VISIBLE);
@@ -92,6 +113,7 @@ public class EarthquakeActivity extends AppCompatActivity {
 
     private void updateUi(ArrayList<Earthquake> result) {
         // Lookup the RecyclerView in activity layout
+
         RecyclerView rvQuake = (RecyclerView) findViewById(R.id.earthquake_list);
         progressBar = (ProgressBar) findViewById(R.id.progress_bar);
         emptyView = (TextView) findViewById(R.id.empty_view);
